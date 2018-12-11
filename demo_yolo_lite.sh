cadab029f4a68b4d879cae3580e154d695686f31
Ming-Yang Liu <eric612kimo@yahoo.com.tw>
2018-12-11 14:48:47 +0800

    Change command path

--- demo_yolo_lite.sh
+++ demo_yolo_lite.sh
@@ -1,1 +1,1 @@
-./build/examples/ssd/ssd_detect models/yolov3/mobilenet_yolov3_lite_deploy.prototxt models/yolov3/mobilenet_yolov3_lite_deploy.caffemodel -file_type image -wait_time 3000 -mean_value 0.5,0.5,0.5 -normalize_value 0.007843 -confidence_threshold 0.3 
+./build/yolo/yolo_detect models/yolov3/mobilenet_yolov3_lite_deploy.prototxt models/yolov3/mobilenet_yolov3_lite_deploy.caffemodel -resize_mode 0 -cpu_mode gpu -file_type image -wait_time 3000 -mean_value 0.5,0.5,0.5 -normalize_value 0.007843 -confidence_threshold 0.3 
