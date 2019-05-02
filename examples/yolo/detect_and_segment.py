2533f178f85714eafeb8be32394dfed387b1d3b0
Ming-Yang Liu <eric612kimo@yahoo.com.tw>
2019-05-02 11:07:18 +0800

    Add bdd100k pelee python example

---
+++ examples/yolo/detect_and_segment.py
@@ -0,0 +1,157 @@
+# set up Python environment: numpy for numerical routines, and matplotlib for plotting
+import numpy as np
+import sys
+import cv2
+from pascal_voc_writer import Writer
+#import matplotlib.pyplot as plt
+# display plots in this notebook
+import argparse
+# set display defaults
+#plt.rcParams['figure.figsize'] = (10, 10)        # large images
+#plt.rcParams['image.interpolation'] = 'nearest'  # don't interpolate: show square pixels
+#plt.rcParams['image.cmap'] = 'gray'  # use grayscale output rather than a (potentially misleading) color heatmap
+
+# The caffe module needs to be on the Python path;
+#  we'll add it here explicitly.
+import sys
+caffe_root = './'  # this file should be run from {caffe_root}/examples (otherwise change this line)
+sys.path.insert(0, caffe_root + 'python')
+import os
+import caffe
+import math
+from os import walk
+from os.path import join
+# If you get "No module named _caffe", either you have not built pycaffe or you have the wrong path.
+
+CLASSES = ('__background__',
+           'person', 'rider', 'car', 'bus',
+           'truck', 'bike', 'motor', 'traffic light', 'traffic sign',
+           'train')
+
+
+def vis_detections(image,result,result2) :
+    w = image.shape[1]
+    h = image.shape[0]
+    seg_w = result2.shape[1]
+    seg_h = result2.shape[2]
+    pic1 = cv2.resize(result2[0,:,:], (w, h), interpolation=cv2.INTER_LINEAR)
+    pic2 = cv2.resize(result2[1,:,:], (w, h), interpolation=cv2.INTER_LINEAR)
+    for i in range(h):
+      for j in range(w):
+        if(pic1[i,j]>0.45) :
+          image[i,j,0]=255
+        if(pic2[i,j]>0.45) :
+          image[i,j,2]=255
+
+    for i in range(result.shape[1]):
+        left = result[0][i][3] * w
+        top = result[0][i][4] * h
+        right = result[0][i][5] * w
+        bot = result[0][i][6] * h
+        score = result[0][i][2]
+        label = result[0][i][1]
+        if(score>0.5) :
+            print(left,right,top,bot,score,label)
+            cv2.rectangle(image,(int(left), int(top)),(int(right),int(bot)),(0,255,0), 2)
+
+            label = '{:s} {:.3f}'.format(CLASSES[int(label)], score)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            size = cv2.getTextSize(label, font, 0.5, 0)[0]
+            cv2.rectangle(image,(int(left), int(top)),
+                    (int(left+size[0]),int(top+ size[1])),(0,255,0), -1)
+
+            cv2.putText(image, label,(int(left+0.5), int(top+ size[1]+0.5)),font,0.5,(0,0,0),0)
+def write_detections(image,result,writer) :
+    w = image.shape[1]
+    h = image.shape[0]
+    for i in range(result.shape[1]):
+        left = result[0][i][3] * w
+        top = result[0][i][4] * h
+        right = result[0][i][5] * w
+        bot = result[0][i][6] * h
+        score = result[0][i][2]
+        label = result[0][i][1]
+        if(score>0.1) :
+            print(left,right,top,bot,score,label)
+            cv2.rectangle(image,(int(left), int(top)),(int(right),int(bot)),(0,255,0), 2)
+
+            label = '{:s}'.format(CLASSES[int(label)])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            size = cv2.getTextSize(label, font, 0.5, 0)[0]
+            cv2.rectangle(image,(int(left), int(top)),
+                    (int(left+size[0]),int(top+ size[1])),(0,255,0), -1)
+
+            cv2.putText(image, label,(int(left), int(top+ size[1])),font,0.5,(0,0,0),0)
+            writer.addObject(label, int(left+0.5), int(top+0.5), int(right+0.5), int(bot+0.5))
+def det(image,transformer,net):
+    
+    transformed_image = transformer.preprocess('data', image)
+    #plt.imshow(image)
+
+    net.blobs['data'].data[...] = transformed_image
+
+    ### perform classification
+    output = net.forward()
+
+    res = output['detection_out'][0]  # the output probability vector for the first image in the batch
+    res2 = output['sigmoid'][0]  # the output probability vector for the first image in the batch
+    #print(res.shape)
+    return res,res2
+
+def is_imag(filename):
+    return filename[-4:] in ['.png', '.jpg']
+
+def main(args):    
+ 
+    caffe.set_mode_cpu()
+    model_def = args.model_def
+    model_weights = args.model_weights
+    
+    net = caffe.Net(model_def,      # defines the structure of the model
+                    model_weights,  # contains the trained weights
+                    caffe.TEST)     # use test mode (e.g., don't perform dropout)
+
+
+    mu = np.array([1.751, 1.983, 2.10])
+    # create transformer for the input called 'data'
+    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+
+    transformer.set_transpose('data', (2,0,1))  # move image channels to outermost dimension
+    transformer.set_mean('data', mu)            # subtract the dataset-mean value in each channel
+    transformer.set_raw_scale('data', 4.335)      # rescale from [0, 1] to [0, 255]
+    transformer.set_channel_swap('data', (2,1,0))  # swap channels from RGB to BGR
+
+    net.blobs['data'].reshape(1,        # batch size
+                              3,         # 3-channel (BGR) images
+                              args.image_resize, args.image_resize)  # image size is 227x227
+    
+    filenames = os.listdir(args.image_dir)
+    images = filter(is_imag, filenames)
+    for image in images :
+        pic = args.image_dir + image
+        input = caffe.io.load_image(pic)       
+        image_show =cv2.imread(pic)  
+        result,result2 = det(input,transformer,net)
+        vis_detections(image_show,result,result2)
+        if args.write_voc:
+            writer = Writer(pic, input.shape[1], input.shape[0])
+            write_detections(image_show,result,writer)
+            base = os.path.splitext(pic)[0]
+            writer.save(base+".xml")
+        else :
+            print('');
+            cv2.imshow("Image", image_show)
+            cv2.waitKey (1000)
+        
+def parse_args():
+    parser = argparse.ArgumentParser()
+    '''parse args'''
+    parser.add_argument('--image_dir', default='data/bdd100k/')
+    parser.add_argument('--model_def', default='models/pelee/deploy.prototxt')
+    parser.add_argument('--model_weights', default='models/pelee/pelee_SSD_304x304_iter_160000.caffemodel')
+    parser.add_argument('--image_resize', default=304, type=int)
+    parser.add_argument('--write_voc', default=False)
+    return parser.parse_args()
+    
+if __name__ == '__main__':
+    main(parse_args())
