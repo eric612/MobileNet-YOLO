0bac1b36e90ca7217ac48bc52ef483dc27463305
Ivan Derianto <32887298+ivder@users.noreply.github.com>
2019-07-25 13:26:07 +0900

    Update yolo_detect.cpp

--- examples/yolo/yolo_detect.cpp
+++ examples/yolo/yolo_detect.cpp
@@ -804,7 +804,7 @@ int main(int argc, char** argv) {
             pt2.x = (img.cols*d[5]);
             pt2.y = (img.rows*d[6]);
 
-            cv::rectangle(img, pt1, pt2, cvScalar(0, 255, 0), 1, 8, 0);
+            cv::rectangle(img, pt1, pt2, cv::Scalar(0, 255, 0), 1, 8, 0);
 
             char label[100];
             sprintf(label, "%s,%f", YOLO_CLASSES[static_cast<int>(d[1])], score);
@@ -813,7 +813,7 @@ int main(int argc, char** argv) {
             cv::Point pt3;
             pt3.x = pt1.x + size.width;
             pt3.y = pt1.y - size.height;
-            cv::rectangle(img, pt1, pt3, cvScalar(0, 255, 0), -1);
+            cv::rectangle(img, pt1, pt3, cv::Scalar(0, 255, 0), -1);
 
             cv::putText(img, label, pt1, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
           }
@@ -920,7 +920,7 @@ int main(int argc, char** argv) {
             int green = 255 * ((index + 1) % 3);
             int blue = 255 * (index % 3);
             int red = 255 * ((index + 1) % 2);
-            cv::rectangle(img, pt1, pt2, cvScalar(red, green, blue), 1, 8, 0);
+            cv::rectangle(img, pt1, pt2, cv::Scalar(red, green, blue), 1, 8, 0);
 
             char label[100];
             sprintf(label, "%s,%f", YOLO_CLASSES[static_cast<int>(d[1])], score);
@@ -930,7 +930,7 @@ int main(int argc, char** argv) {
             pt3.x = pt1.x + size.width;
             pt3.y = pt1.y - size.height;
 
-            cv::rectangle(img, pt1, pt3, cvScalar(red, green, blue), -1);
+            cv::rectangle(img, pt1, pt3, cv::Scalar(red, green, blue), -1);
 
             cv::putText(img, label, pt1, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
 
