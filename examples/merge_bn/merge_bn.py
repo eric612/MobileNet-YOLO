e1979d0ff261bb8488b46b672c76ae426bbb6631
Eric Liu <ericliu612@gmail.com>
2019-10-23 10:45:08 +0800

    Update merge_bn

--- examples/merge_bn/merge_bn.py
+++ examples/merge_bn/merge_bn.py
@@ -20,16 +20,21 @@ def merge_bn(net, nob):
     '''
     for key in net.params.iterkeys():
         if type(net.params[key]) is caffe._caffe.BlobVec:
-            if key.endswith("/bn") or key.endswith("/scale"):
+            if key.endswith("/bn") or key.endswith("/scale") or 'batch_norm' in key or 'bn_scale' in key:
 		continue
             else:
                 conv = net.params[key]
-                if not net.params.has_key(key + "/bn"):
+                print(key)
+                if not net.params.has_key(key + "/bn") :
                     for i, w in enumerate(conv):
                         nob.params[key][i].data[...] = w.data
                 else:
+
                     bn = net.params[key + "/bn"]
                     scale = net.params[key + "/scale"]
+                    #bn = net.params[key.replace('conv','batch_norm')]
+                    #scale = net.params[key.replace('conv','bn_scale')]
+                    #print(bn)
                     wt = conv[0].data
                     channels = wt.shape[0]
                     bias = np.zeros(wt.shape[0])
@@ -46,7 +51,7 @@ def merge_bn(net, nob):
                         scalef = 1. / scalef
                     mean = mean * scalef
                     var = var * scalef
-                    rstd = 1. / np.sqrt(var + 1e-5)
+                    rstd = 1. / np.sqrt(var + 9.999999747378752e-06)
                     rstd1 = rstd.reshape((channels,1,1,1))
                     scales1 = scales.reshape((channels,1,1,1))
                     wt = wt * rstd1 * scales1
--- examples/merge_bn/remove_bn.py
+++ examples/merge_bn/remove_bn.py
@@ -8,35 +8,113 @@ from caffe import params as P
 import caffe
 net = caffe_pb2.NetParameter()
 from google.protobuf import text_format
+net2 = caffe_pb2.NetParameter()
 #fn = 'mobilenet_yolov3_test.prototxt'
+
+def Data(name, tops, source, batch_size, phase):
+    layer = caffe_pb2.LayerParameter()
+    layer.name = name
+    layer.type = 'Data'
+    layer.top.extend(tops)
+    layer.data_param.source = source
+    layer.data_param.batch_size = batch_size
+    layer.data_param.backend = caffe_pb2.DataParameter.LMDB
+
+    return layer
+    
 def remove_bn_prototxt(fn,filename) :
     with open(fn) as f:
         s = f.read()
         txtf.Merge(s, net)
 
     net.name = 'remove_bn'
+    net2.name = 'remove_bn'
+    data = net2.layer.add()
+    
+    #layers = []
+    #layers.append(Data('data', ['data', 'label'],
+    #                   'examples/imagenet/ilsvrc12_train_lmdb', 32, 'train'))
+                       
+    #data.CopyFrom(layers)
     #layerNames = [l.name for l in net.layer]
     idx = 0
     del_list = list()
     #print(len(net.layer))
+    top = 0 
+    delflag = False
     while idx < len(net.layer) :
-
-        if 'bn' in net.layer[idx].name :
-            print('delete %s'%net.layer[idx].name)
+        #print(idx)
+        #print(net.layer[idx].type)
+        if 'BatchNorm' in net.layer[idx].type :
+            #print('delete %s'%net.layer[idx].name)
+            #if 'ReLU' not in net.layer[idx+2].type :
+                #print(net.layer[idx-1].name)
+            #    net.layer[idx].bottom[0] = top
+            #else :
+            #    top = net.layer[idx].bottom[0]
+            #print(net.layer[idx+2].type)
+            #top = net.layer[idx].bottom[0]
             del net.layer[idx]
-        elif 'scale' in net.layer[idx].name :
-            print('delete %s'%net.layer[idx].name)
+            delflag = True
+        elif 'Scale' in net.layer[idx].type :
+            #print('delete %s'%net.layer[idx].name)
+            #top = net.layer[idx].bottom[0]
             del net.layer[idx]
-        elif 'conv' in net.layer[idx].name and 'relu' not in net.layer[idx].name and 'sum' not in net.layer[idx].name:
+            #delflag = True
+        elif 'Convolution' in net.layer[idx].type and 'ReLU' not in net.layer[idx].type :
             #print(net.layer[idx].convolution_param.bias_term)
             #net.layer[idx].include.remove(net.layer[idx].param[0])
-            #print(net.layer[idx].include.remove)
-            net.layer[idx].param[0].lr_mult = int(1)
-            net.layer[idx].param[0].decay_mult = int(1)
-            net.layer[idx].convolution_param.bias_term = True
+            #if 'ReLU' not in net.layer[idx-1].type :
+            #    print(net.layer[idx-1].name)
+            if delflag == True :
+                net.layer[idx].bottom[0] = top
+            #net.layer[idx].param[0].lr_mult = int(1)
+            #net.layer[idx].param[0].decay_mult = int(1)
+            top = net.layer[idx].top[0]
+            #print(net.layer[idx].name)
+            net.layer[idx].convolution_param.bias_term = True         
+            #if delflag == True :
+            #    net.layer[idx].bottom[0] = top
+            #print(top)
             idx+=1
+            delflag = False
+            '''
+        elif 'data' in net.layer[idx].name :
+            print('delete %s'%net.layer[idx].name)
+            #del net.layer[idx]
+            #data = net.layer.add()
+            net.layer[idx].type = 'data'
+            del net.layer[idx].top[1]
+            del net.layer[idx].include
+            #net.layer[idx].include.remove(net.layer[idx].include[0])
+            #net.layer[idx].transform_param.remove()
+            #data.top[0] = 'data'
+            #net.layer[idx].CopyFrom(L.Data(net,ntop=1))
+            idx += 1
+            '''
+        elif 'Power' in net.layer[idx].type :
+            if delflag == True :
+                net.layer[idx].bottom[0] = top
+            idx+=1
+        elif 'Eltwise' in net.layer[idx].type :
+            if delflag == True : #for mobilenetv2
+                if 'batch_norm_blob' in net.layer[idx].bottom[0] :
+                    #print(net.layer[idx].bottom[0])
+                    net.layer[idx].bottom[0] = net.layer[idx].bottom[0].replace('batch_norm','conv');
+                net.layer[idx].bottom[1] = top
+            idx+=1
+            delflag = False
+        elif 'Concat' in net.layer[idx].type :
+            #if delflag == True :
+            #    net.layer[idx].bottom[0] = top
+            idx+=1
+            delflag = False
         else :
+            if delflag == True :
+                net.layer[idx].bottom[0] = top
+            #print(top)
             idx+=1
+            delflag = False
 
     outFn = filename
     print 'writing', outFn
